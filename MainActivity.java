e3acbc4c4bc1ae2f5bfb54c5265d6738501784f0
jaishri1204 <101888800+jaishri1204@users.noreply.github.com>
2022-03-22 22:05:59 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.sample_1;
+package com.example.myapp;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void verify(View view) {
+        Intent login = new Intent(this, MainActivity2.class);
+        startActivity(login);
     }
 }
